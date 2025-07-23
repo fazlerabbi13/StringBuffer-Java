7e2436ccd41e169cd636bacb9423cc5d8bec5371
fazlerabbi13 <mohammad0132794@gmail.com>
2025-07-23 11:52:31 +0600

    It can reverse the characters within a StringBuffer object using reverse( ). This method returns the reversed object on which it was called.

---
+++ Reverse.java
@@ -0,0 +1,8 @@
+public class Reverse {
+    public static void main(String[] args) {
+        StringBuffer sb = new StringBuffer("Hello");
+
+        sb.reverse();
+        System.out.println(sb);
+    }
+}
