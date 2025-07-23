61c94266b8cddd73fa97f65e1fb66fb836f562dd
fazlerabbi13 <mohammad0132794@gmail.com>
2025-07-23 11:47:40 +0600

    Replace one set of characters with another set inside a StringBuffer object.

---
+++ Replace.java
@@ -0,0 +1,10 @@
+
+public class Replace{
+    public static void main(String[] args) {
+        StringBuffer sb = new StringBuffer("Fazlefgh");
+
+        sb.replace(5, 8, "Rabbi");
+
+        System.out.println(sb);
+    }
+}
