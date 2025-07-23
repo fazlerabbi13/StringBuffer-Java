c45b374cf96cd5a84eb90d8b4dae5e609fefec22
fazlerabbi13 <mohammad0132794@gmail.com>
2025-07-23 11:56:49 +0600

    The length of a StringBuffer can be found by the length( ) method.

---
+++ Length.java
@@ -0,0 +1,9 @@
+public class Length {
+    public static void main(String[] args) {
+        StringBuffer sb = new StringBuffer("Hello World");
+
+        int l= sb.length();
+
+        System.out.println(l);
+    }
+}
