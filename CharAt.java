176f3446e599f75c4f62e1a9e7cd532ef5514f5b
fazlerabbi13 <mohammad0132794@gmail.com>
2025-07-23 15:49:08 +0600

    String charAt() method in Java returns the character at the specified index in a string.

---
+++ CharAt.java
@@ -0,0 +1,11 @@
+public class CharAt {
+    public static void main(String[] args) {
+        
+        String s = "i am in 1% club";
+
+        char ch = s.charAt(9);
+
+        System.out.println(ch);
+
+    }
+}
