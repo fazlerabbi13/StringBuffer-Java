ef690b73bca893374042905e12d2dfc396c7d9eb
fazlerabbi13 <mohammad0132794@gmail.com>
2025-07-23 15:57:28 +0600

    Deletes the character at the index specified by the location.

---
+++ DeleteCharAt.java
@@ -0,0 +1,10 @@
+public class DeleteCharAt {
+    public static void main(String[] args) {
+        StringBuffer sb = new StringBuffer("HelloWWorld");
+
+        sb.deleteCharAt(5);
+
+        System.out.println(sb);
+
+    }
+}
